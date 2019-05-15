38ec69592b5c187d101f19798ce4314f78a6d79e
Brittany Alleman <brittany.alleman1@gmail.com>
2019-05-15 16:10:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,6 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   add_element_to_end_of_array = ("array" "element")
   add_element_to_end_of_array.push ("arrays!")
+  add_element_to_end_of_array.inspect
 end
 
 def add_element_to_start_of_array(array, element)
