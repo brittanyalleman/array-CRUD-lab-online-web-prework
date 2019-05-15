e45fbd4822b85fca777c1115d643c63832001894
Brittany Alleman <brittany.alleman1@gmail.com>
2019-05-15 16:56:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,12 +19,11 @@ array.pop
 end
 
 def remove_element_from_start_of_array(array)
-array.shift 
+array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  add_element_to_end_of_array = ["array","element"]
-  add_element_to_end_of_array[1]
+add_element_to_end_of_array[1]
 end
 
 def retrieve_first_element_from_array(array)
