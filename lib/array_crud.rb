3ac1bb7b2ad6a11ad88a77c862881edeb82d6dd0
Brittany Alleman <brittany.alleman1@gmail.com>
2019-05-15 16:50:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,9 +15,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  add_element_to_end_of_array = ["array","element"]
-  add_element_to_end_of_array.pop ("arrays!")
-  add_element_to_end_of_array.inspect
+array.pop (element)
 end
 
 def remove_element_from_start_of_array(array)
